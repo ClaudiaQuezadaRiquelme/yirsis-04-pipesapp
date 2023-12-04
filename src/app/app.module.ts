a4fdd187c39bf269715eb1a31ee53b519692028a
claudia.quezada <claudia.quezada@tisal.cl>
2023-12-04 10:49:58 -0300

    custom pipe darkMode

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { PipesCommonModule } from './components/pipes-common/pipes-common.module';
 import { CustomPipesModule } from './components/custom-pipes/custom-pipes.module';
+import { DarkModePipe } from './shared/pipes/dark-mode.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DarkModePipe,
   ],
   imports: [
     BrowserModule,
--- src/app/components/custom-pipes/custom-pipes.module.ts
+++ src/app/components/custom-pipes/custom-pipes.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CustomContainerComponent } from './custom-container/custom-container.component';
-import { DarkModePipe } from 'src/app/shared/pipes/dark-mode.pipe';
 
 
 
 @NgModule({
   declarations: [
     CustomContainerComponent,
-    DarkModePipe,
   ],
   imports: [
     CommonModule,
