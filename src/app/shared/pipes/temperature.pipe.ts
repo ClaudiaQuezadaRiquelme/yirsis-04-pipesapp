e60f7fad54b36689629db8d3170a0cd7462b1bd6
claudia.quezada <claudia.quezada@tisal.cl>
2023-12-04 11:30:38 -0300

    temperature pipe fixed decimals

--- src/app/shared/pipes/temperature.pipe.ts
+++ src/app/shared/pipes/temperature.pipe.ts
@@ -6,7 +6,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 export class TemperaturePipe implements PipeTransform {
 
   transform(value: number, type: string): string {
-    const final = (type === 'F' || type === 'f') ? (((value * 9)/5) + 32) : (value);
+    const final = ((type === 'F' || type === 'f') ? ((value * (9/5)) + 32) : (value)).toFixed(1);
     return `${final} °${type}`;
   }
 
