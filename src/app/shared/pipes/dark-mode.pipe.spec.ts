040faf3d6d7006dda5a09d277df52267dfe24cf9
claudia.quezada <claudia.quezada@tisal.cl>
2023-12-11 14:10:57 -0300

    test dark-mode pipe

--- src/app/shared/pipes/dark-mode.pipe.spec.ts
+++ src/app/shared/pipes/dark-mode.pipe.spec.ts
@@ -2,7 +2,12 @@ import { DarkModePipe } from "./dark-mode.pipe";
 
 describe('DarkModePipe', ()=> {
 
-    it('Debería retornar DarkMode si mando true', ()=> {
+    it('Debería retornar DarkMode  si mando true', ()=> {
         const pipe = new DarkModePipe();
+        expect(pipe.transform(true)).toEqual('bg-dark text-white');
+    });
+    it('Debería retornar LightMode si mando false', ()=> {
+        const pipe = new DarkModePipe();
+        expect(pipe.transform(false)).toEqual('bg-light text-dark');
     });
 });
